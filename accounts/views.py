f1375de6dd1f1bc281707f2a503071a4019b96ef
Esteban Gonzalez <esteban-g-7@homail.com>
2025-09-25 23:52:22 -0500

    add views.py from previous repo

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
 # Create your views here.
+def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # automatically log in the new user
+            return redirect("/")  # redirect to home page or dashboard
+    else:
+        form = SignUpForm()
+    return render(request, "registration/signup.html", {"form": form})
