3a61871c104c216a7c965832a9ba607a22d0f660
Esteban Gonzalez <esteban-g-7@homail.com>
2025-09-29 18:30:29 -0500

    Update project URLs to include reviews app routes

--- aurea_project/urls.py
+++ aurea_project/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('bookings/', include('bookings.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path("accounts/", include("accounts.urls")),
-    path("accounts/", include("django.contrib.auth.urls"))
+    path('reviews/', include('reviews.urls')),
 ]
