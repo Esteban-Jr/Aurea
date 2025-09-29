4739f03b7f6bac4562bd2f09c6cb9443651765a3
Esteban Gonzalez <esteban-g-7@homail.com>
2025-09-29 18:24:31 -0500

    chore(reviews/admin): register Review model in admin with filters and search

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display  = ('user', 'rating', 'approved', 'created_at')
+    list_filter   = ('approved', 'rating', 'created_at')
+    search_fields = ('user__username', 'comment')
+    ordering      = ('-created_at',)
+
+    def star_rating(self, obj):
+        return '★' * obj.rating + '☆' * (5 - obj.rating)
+    star_rating.short_description = "Rating"
