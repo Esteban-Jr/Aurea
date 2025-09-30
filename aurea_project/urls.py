80117eb0f2201d59d7da4665f482e0e537d14665
Esteban Gonzalez <esteban-g-7@homail.com>
2025-09-30 00:41:23 -0500

    remove unneccesary line

--- aurea_project/urls.py
+++ aurea_project/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bookings/', include('bookings.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
     path("accounts/", include("accounts.urls")),
     path('reviews/', include('reviews.urls')),
+    path("", include("pages.urls")), 
 ]
