68c6010ff48f915929489d9e925d630fa32ad0fa
Esteban Gonzalez <esteban-g-7@homail.com>
2025-10-01 04:14:36 -0500

    Routes: add edit and delete booking URLs

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('book/', views.create_bookings, name='create_bookings'),
     path('booking-success/', views.booking_success, name='booking_success'),
-    #edit booking
-    #delete booking
+    path('<int:pk>/edit/', views.edit_booking, name='edit_booking'),
+    path('<int:pk>/delete/', views.delete_booking, name='delete_booking'),
 ]
