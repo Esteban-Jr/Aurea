1627b11a2f5a77c806225cdc3914491f38f8b0ae
Esteban Gonzalez <esteban-g-7@homail.com>
2025-10-01 17:18:09 -0500

    Extend SignUpForm to include first_name and last_name

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,13 +3,18 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, help_text="Enter your first name")
+    last_name = forms.CharField(max_length=30, required=True, help_text="Enter your last name")
     email = forms.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField(required=True)
+
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name')
