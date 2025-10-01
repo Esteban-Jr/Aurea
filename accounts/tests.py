a540c7061f6b099352a74578368bd369704e3cdf
Esteban Gonzalez <esteban-g-7@homail.com>
2025-10-01 02:40:18 -0500

    remove unused feature

--- accounts/tests.py
+++ accounts/tests.py
@@ -13,38 +13,3 @@ class PasswordResetTests(TestCase):
             email="tester@example.com",
             password="oldpassword123"
         )
-
-    def test_password_reset_flow(self):
-        # 1. Request reset
-        response = self.client.post(reverse("password_reset"), {
-            "email": "tester@example.com"
-        })
-        self.assertRedirects(response, reverse("password_reset_done"))
-        self.assertEqual(len(mail.outbox), 1)
-
-        # 2. Build a valid reset link
-        uid = urlsafe_base64_encode(force_bytes(self.user.pk))
-        token = default_token_generator.make_token(self.user)
-        reset_link = reverse("password_reset_confirm", kwargs={
-            "uidb64": uid,
-            "token": token
-        })
-
-        # 3. Open reset link (allow 200 or 302)
-        response = self.client.get(reset_link)
-        self.assertIn(response.status_code, [200, 302])
-
-        if response.status_code == 200:
-            # 4. Post new password
-            response = self.client.post(reset_link, {
-                "new_password1": "newsecurepassword123",
-                "new_password2": "newsecurepassword123",
-            })
-            self.assertRedirects(response, reverse("password_reset_complete"))
-
-            # 5. Verify login works with new password
-            login_success = self.client.login(
-                username="tester",
-                password="newsecurepassword123"
-            )
-            self.assertTrue(login_success)
