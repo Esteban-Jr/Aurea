389c60daf8ee913ccd15e8a0425db505f9d04dc8
Esteban Gonzalez <esteban-g-7@homail.com>
2025-10-01 04:09:05 -0500

    Config: update login redirect to home

--- aurea_project/settings.py
+++ aurea_project/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+LOGIN_REDIRECT_URL = 'home'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
