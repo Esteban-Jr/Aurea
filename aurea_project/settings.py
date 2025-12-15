d2e4a1dd787f091bf7207eea2f550be04aec5b2c
Esteban Gonzalez <esteban-g-7@homail.com>
2025-12-15 20:03:00 +0000

    Security: move SECRET_KEY to environment variables

--- aurea_project/settings.py
+++ aurea_project/settings.py
@@ -12,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kx82zwwn!_478&w7cb$dt+75u^j)g1ebkfczul!u3_)!j1-9)a'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aurea-project-53c55f91b857.herokuapp.com', '127.0.0.1']
 
