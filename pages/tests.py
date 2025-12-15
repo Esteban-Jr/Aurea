f278c7e0b5855ae68f4bbcf17f37af46e8e091ab
Esteban Gonzalez <esteban-g-7@homail.com>
2025-12-15 21:43:07 +0000

    Test: add automated tests for pages views and 404 edge case

--- pages/tests.py
+++ pages/tests.py
@@ -1,13 +1,33 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 class PagesTests(TestCase):
-    def test_about_page(self):
+    def test_home_page_renders(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/home.html")
+
+    def test_about_page_renders(self):
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "pages/about.html")
 
-    def test_home_page(self):
-        response = self.client.get(reverse("home"))
+    def test_menu_page_renders(self):
+        response = self.client.get(reverse("menu"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "pages/home.html")
+        self.assertTemplateUsed(response, "pages/menu.html")
+
+    def test_faq_page_renders(self):
+        response = self.client.get(reverse("faq"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/faq.html")
+
+    def test_gallery_page_renders(self):
+        response = self.client.get(reverse("gallery"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/gallery.html")
+
+    def test_invalid_page_returns_404_edge_case(self):
+        response = self.client.get("/this-page-does-not-exist/")
+        self.assertEqual(response.status_code, 404)
